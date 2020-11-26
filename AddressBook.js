f0c08c3ea7cf77e3d5e34cde0fb1181249508c76
Saurabh <saurabh.nit20@gmail.com>
2020-11-26 10:22:11 +0530

    [Saurabh].Addition of Contact in AddressBook

--- AddressBook.js
+++ AddressBook.js
@@ -1,10 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿
+//UC1 : Creation of Contact
 
-namespace AddressBook_JS
-{
-    class AddressBook
-    {
-    }
+function Contact(firstName, lastName, address, city, state, zip, phone, email) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.address = address;
+    this.city = city;
+    this.state = state;
+    this.zip = zip;
+    this.phone = phone;
+    this.email = email;
+    validate.validateAll(this);
 }
